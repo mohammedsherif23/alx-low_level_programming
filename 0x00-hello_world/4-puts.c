562171b5e04d0b461ae7d53e8e498611e296f054
root <sherifmohammed634@gmail.com>
2023-02-16 12:40:33 -0800

    my first C program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - The main function
+ *  Writing my first C program
+ *  Return: 0 (Success)
+ */
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
