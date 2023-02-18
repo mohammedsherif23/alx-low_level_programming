d2fb740d7dbe6a23258e12432e28b6881b32c0ce
root <sherifmohammed634@gmail.com>
2023-02-17 18:16:49 -0800

    Write a program to print last digit of a number

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - Program Entry point
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	int i;
+
+	i = n % 10;
+	if (i > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, i);
+	} else if (i < 6 && i != 0)
+	{
+		printf("Last digit of %d is %d and is not 0\n", n, i);
+	} else if (i == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, i);
+	}
+	return (0);
+}
