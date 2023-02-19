29c90615c8bcd7025dbea0f7589685f4c2bd2256
root <sherifmohammed634@gmail.com>
2023-02-19 02:47:42 -0800

    6-print_numberz.c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - Program Entry point
+* Return: 0 (success)
+*/
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar('0' + n);
+	}
+	putchar('\n');
+	return (0);
+}
