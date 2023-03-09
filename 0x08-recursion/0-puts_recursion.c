13d4020567ccfaef1da177df23a2326c59c67023
root <sherifmohammed634@gmail.com>
2023-03-08 18:21:04 -0800

    printing string

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* _puts_recursion - a program to print a string
+*@s: - string to be printed
+* Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
