77c74b41364b4e606045c6b9ae7335edcb015d02
root <sherifmohammed634@gmail.com>
2023-03-15 03:18:29 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - main function
+* @argc: number of arguments
+* @argv: array of arguments
+* Return: 0 always
+*/
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("argc = %d\n", argc);
+	return (0);
+}
