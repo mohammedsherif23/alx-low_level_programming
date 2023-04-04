6e676adbe7fbcb7b81856d039215624fbdfd73c1
root <sherifmohammed634@gmail.com>
2023-04-04 11:44:04 -0700

    return number of elements in a linked

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+* listint_len - retrun the number of elements in a listint_t
+* @h: input
+* Return: 0 always
+*/
+
+size_t listint_len(const listint_t *h)
+{
+        size_t count = 0;
+
+        while (h != NULL)
+        {
+                count++;
+                h = h->next;
+        }
+        return (count);
+}
